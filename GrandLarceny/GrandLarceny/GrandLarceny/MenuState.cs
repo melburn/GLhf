a21c7f251fe53377c189efc0fbee0b6f508f8b22
Joakim Clysen <stjoakim@gmail.com>
2012-01-24 13:24:28 +0100

    GameState + GameObject

--- GrandLarceny/GrandLarceny/GrandLarceny/MenuState.cs
+++ GrandLarceny/GrandLarceny/GrandLarceny/MenuState.cs
@@ -2,10 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace GrandLarceny
 {
 	class MenuState : States
 	{
+		public override void update(GameTime a_gameTime)
+		{
+
+		}
+
+		public override void draw(GameTime a_gameTime, SpriteBatch a_spriteBatch)
+		{
+
+		}
 	}
 }
