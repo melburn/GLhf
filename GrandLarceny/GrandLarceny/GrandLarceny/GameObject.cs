0275e470b0f8b396c176251c12b0c21c57785085
Melburn <Melburn1@hotmail.com>
2012-01-24 13:23:35 +0100

    la till GameObject igen

---
+++ GrandLarceny/GrandLarceny/GrandLarceny/GameObject.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace GrandLarceny
+{
+	class GameObject
+	{
+		private bool m_dead = false;
+        private Position m_myPos;
+
+        public GameObject(int a_posX, int a_posY)
+        {
+            m_myPos = new Position(a_posX, a_posY);
+        }
+
+		public virtual void update(GameTime a_gameTime)
+		{
+			
+		}
+
+		public virtual void draw(GameTime a_gameTime)
+		{
+
+		}
+		public bool isDead()
+		{
+			return m_dead;
+		}
+	}
+}
