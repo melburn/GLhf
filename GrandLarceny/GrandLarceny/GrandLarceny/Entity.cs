4028fbdec3eb1656f2b2a196361b32ab759264e1
Melburn <Melburn1@hotmail.com>
2012-01-24 13:42:11 +0100

    fixat sa att den inte ger kompfel

--- GrandLarceny/GrandLarceny/GrandLarceny/Entity.cs
+++ GrandLarceny/GrandLarceny/GrandLarceny/Entity.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
 
 namespace GrandLarceny
 {
 	class Entity : GameObject
 	{
+		public Entity(Vector2 a_posV2)
+			: base(a_posV2)
+		{
+		}
 	}
 }
