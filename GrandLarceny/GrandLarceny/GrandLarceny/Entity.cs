31e08d43b1d94b92500c10ed27024269fe818d77
Yuma Shimizu <yumashimizu90@gmail.com>
2012-01-25 21:48:12 +0100

    addade ramverk for player och entity samt addade saknande bild

--- GrandLarceny/GrandLarceny/GrandLarceny/Entity.cs
+++ GrandLarceny/GrandLarceny/GrandLarceny/Entity.cs
@@ -8,9 +8,20 @@ namespace GrandLarceny
 {
 	class Entity : GameObject
 	{
-		public Entity(Vector2 a_posV2)
-			: base(a_posV2, null)
+		public Entity(Vector2 a_posV2, String a_sprite)
+			: base(a_posV2, a_sprite)
 		{
+
 		}
+
+        public virtual override void update(GameTime a_gameTime)
+        {
+
+        }
+
+        public virtual override void draw(GameTime a_gameTime)
+        {
+
+        }
 	}
 }
--- GrandLarceny/GrandLarceny/GrandLarceny/Player.cs
+++ GrandLarceny/GrandLarceny/GrandLarceny/Player.cs
@@ -3,14 +3,93 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 
 namespace GrandLarceny
 {
 	class Player : Entity
 	{
-		public Player(Vector2 a_posV2)
-			: base(a_posV2)
+
+        enum State
+        {
+            Stop,
+            Walking,
+            Jumping,
+            Sliding,
+            Climbing
+        }
+        State m_currentState = State.Stop;
+
+		public Player(Vector2 a_posV2, string a_sprite)
+			: base(a_posV2, a_sprite)
 		{
 		}
+
+        public override void update(GameTime a_gameTime)
+        {
+            KeyboardState t_keyInput = Keyboard.GetState();
+
+            switch (m_currentState)
+            {
+                case State.Stop:
+                {
+                    updateStop(t_keyInput);
+                    break;
+                }
+                case State.Walking:
+                {
+                    updateWalking(t_keyInput);
+                    break;
+                }
+                case State.Jumping:
+                {
+                    updateJumping(t_keyInput);
+                    break;
+                }
+                case State.Sliding:
+                {
+                    updateSliding(t_keyInput);
+                    break;
+                }
+                case State.Climbing:
+                {
+                    updateClimbing(t_keyInput);
+                    break;
+                }
+            }
+
+
+        }
+
+        private void updateClimbing(KeyboardState t_keyInput)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void updateSliding(KeyboardState t_keyInput)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void updateJumping(KeyboardState t_keyInput)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void updateWalking(KeyboardState t_keyInput)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void updateStop(KeyboardState t_keyInput)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void draw(GameTime a_gameTime)
+        {
+
+        }
+
 	}
 }
