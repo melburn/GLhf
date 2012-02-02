5775a5b4270beb3a21761f3a4215997068103c2c
Melburn <Melburn1@hotmail.com>
2012-02-02 11:17:46 +0100

    addade guard

---
+++ GrandLarceny/GrandLarceny/GrandLarceny/Guard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace GrandLarceny
+{
+	class Guard : NPE
+	{
+		public Guard(Vector2 a_posV2, String a_sprite)
+			: base(a_posV2, a_sprite)
+		{
+		}
+	}
+}
