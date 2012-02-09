e9fce41507a6947e5b8d59faebf30b262f7635d9
Joakim Clysen <stjoakim@gmail.com>
2012-02-09 11:51:53 +0100

    level editor har nu states och saker sa att man kan placera ut med vansterklick istallet for andra saker, ctrl+s ar save nu

--- GrandLarceny/GrandLarceny/GrandLarceny/DevelopmentState.cs
+++ GrandLarceny/GrandLarceny/GrandLarceny/DevelopmentState.cs
@@ -21,6 +21,16 @@ namespace GrandLarceny
 
 		private SpriteFont m_testFont;
 		private String m_selectedInfo;
+		private Vector2 m_worldMouse;
+
+		private State m_itemToCreate;
+		private enum State
+		{
+			Platform,
+			Background,
+			Ladder,
+			None
+		}
 
 		public DevelopmentState(int a_levelToLoad)
 		{
@@ -38,6 +48,15 @@ namespace GrandLarceny
 		{
 			m_currentKeyboard = Keyboard.GetState();
 			m_currentMouse = Mouse.GetState();
+			
+			m_worldMouse.X = 
+				Mouse.GetState().X / Game.getInstance().m_camera.getZoom()
+				+ (int)Game.getInstance().m_camera.getPosition().getGlobalCartesianCoordinates().X
+				- ((Game.getInstance().m_graphics.PreferredBackBufferWidth / 2) / Game.getInstance().m_camera.getZoom());
+			m_worldMouse.Y = 
+				Mouse.GetState().Y / Game.getInstance().m_camera.getZoom() 
+				+ (int)Game.getInstance().m_camera.getPosition().getGlobalCartesianCoordinates().Y
+				- ((Game.getInstance().m_graphics.PreferredBackBufferHeight / 2) / Game.getInstance().m_camera.getZoom() + 72);
 
 			if (m_selectedObject != null)
 			{
@@ -48,6 +67,7 @@ namespace GrandLarceny
 				m_selectedInfo = "Joxe!";
 			}
 
+			updateCamera();
 			updateKeyboard();
 			updateMouse();
 
@@ -55,33 +75,50 @@ namespace GrandLarceny
 			m_previousMouse = m_currentMouse;
 		}
 
-		private void updateKeyboard()
+		private void updateCamera()
 		{
 			if (m_currentKeyboard.IsKeyDown(Keys.Right) || m_currentMouse.X > Game.getInstance().m_graphics.PreferredBackBufferWidth - 25)
-				Game.getInstance().m_camera.move(new Vector2(10 / Game.getInstance().m_camera.getZoom(), 0));
+				Game.getInstance().m_camera.move(new Vector2(15 / Game.getInstance().m_camera.getZoom(), 0));
 			if (m_currentKeyboard.IsKeyDown(Keys.Left) || m_currentMouse.X < 25)
-				Game.getInstance().m_camera.move(new Vector2(-10 / Game.getInstance().m_camera.getZoom(), 0));
+				Game.getInstance().m_camera.move(new Vector2(-15 / Game.getInstance().m_camera.getZoom(), 0));
 			if (m_currentKeyboard.IsKeyDown(Keys.Up) || m_currentMouse.Y < 25)
-				Game.getInstance().m_camera.move(new Vector2(0, -10 / Game.getInstance().m_camera.getZoom()));
+				Game.getInstance().m_camera.move(new Vector2(0, -15 / Game.getInstance().m_camera.getZoom()));
 			if (m_currentKeyboard.IsKeyDown(Keys.Down) || m_currentMouse.Y > Game.getInstance().m_graphics.PreferredBackBufferHeight - 25)
-				Game.getInstance().m_camera.move(new Vector2(0, 10 / Game.getInstance().m_camera.getZoom()));
-			if (m_currentKeyboard.IsKeyDown(Keys.Z))
+				Game.getInstance().m_camera.move(new Vector2(0, 15 / Game.getInstance().m_camera.getZoom()));
+			if (m_currentMouse.ScrollWheelValue > m_previousMouse.ScrollWheelValue)
 				Game.getInstance().m_camera.zoomIn(0.1f);
-			if (m_currentKeyboard.IsKeyDown(Keys.X))
+			if (m_currentMouse.ScrollWheelValue < m_previousMouse.ScrollWheelValue)
 				Game.getInstance().m_camera.zoomOut(0.1f);
+		}
 
-			if(m_currentKeyboard.IsKeyDown(Keys.S))
+		private void updateKeyboard()
+		{
+			if (m_currentKeyboard.IsKeyDown(Keys.P))
+				m_itemToCreate = State.Platform;
+
+			if (m_currentKeyboard.IsKeyDown(Keys.L))
+				m_itemToCreate = State.Platform;
+
+			if (m_currentKeyboard.IsKeyDown(Keys.B))
+				m_itemToCreate = State.Background;
+
+			if (m_currentKeyboard.IsKeyDown(Keys.D) && m_selectedObject != null) {
+				m_gameObjectList.Remove(m_selectedObject);
+				m_selectedObject = null;
+			}
+
+			if (m_currentKeyboard.IsKeyDown(Keys.LeftControl) && m_currentKeyboard.IsKeyDown(Keys.S) && m_previousKeyboard.IsKeyUp(Keys.S))
 			{
 				Level t_saveLevel = new Level();
 				t_saveLevel.setLevelObjects(m_gameObjectList);
 				Serializer.getInstace().SaveLevel("Level3.txt", t_saveLevel);
 
 			}
-			if(m_currentKeyboard.IsKeyDown(Keys.L))
+			if (m_currentKeyboard.IsKeyDown(Keys.LeftControl) && m_currentKeyboard.IsKeyDown(Keys.O) && m_previousKeyboard.IsKeyUp(Keys.O))
 			{
 				Level t_newLevel = Serializer.getInstace().loadLevel("Level3.txt");
 				m_gameObjectList = t_newLevel.getLevelObjects();
-				foreach(GameObject f_gb in m_gameObjectList)
+				foreach (GameObject f_gb in m_gameObjectList)
 				{
 					f_gb.initImage();
 				}
@@ -90,22 +127,34 @@ namespace GrandLarceny
 
 		private void updateMouse()
 		{
-			Vector2 t_worldMouse;
-			t_worldMouse.X = 
-				Mouse.GetState().X / Game.getInstance().m_camera.getZoom()
-				+ (int)Game.getInstance().m_camera.getPosition().getGlobalCartesianCoordinates().X
-				- ((Game.getInstance().m_graphics.PreferredBackBufferWidth / 2) / Game.getInstance().m_camera.getZoom());
-			t_worldMouse.Y = 
-				Mouse.GetState().Y / Game.getInstance().m_camera.getZoom() 
-				+ (int)Game.getInstance().m_camera.getPosition().getGlobalCartesianCoordinates().Y
-				- ((Game.getInstance().m_graphics.PreferredBackBufferHeight / 2) / Game.getInstance().m_camera.getZoom() + 72);
-
-			if (m_currentMouse.LeftButton == ButtonState.Pressed 
-				&& m_previousMouse.LeftButton == ButtonState.Pressed
-				&& m_selectedObject != null)
+			if (m_currentMouse.LeftButton == ButtonState.Pressed && m_previousMouse.LeftButton == ButtonState.Pressed && m_selectedObject != null) 
+				updateMouseDrag(m_worldMouse);
+			
+			if (m_currentMouse.LeftButton == ButtonState.Pressed && m_previousMouse.LeftButton == ButtonState.Released && m_itemToCreate != State.None)
 			{
-				updateMouseDrag(t_worldMouse);
+				switch (m_itemToCreate)
+				{
+					case State.Background:
+					{
+						createBackground();
+						break;
+					}
+					case State.Ladder:
+					{
+						createLadder();
+						break;
+					}
+					case State.Platform:
+					{
+						createPlatform();
+						break;
+					}
+				}
 			}
+
+			if (m_currentMouse.RightButton == ButtonState.Pressed && m_itemToCreate != State.None)
+				m_itemToCreate = State.None;
+
 			if (m_currentMouse.LeftButton == ButtonState.Pressed && m_previousMouse.LeftButton == ButtonState.Released) 
 			{
 				if (m_selectedObject != null)
@@ -113,7 +162,7 @@ namespace GrandLarceny
 					m_selectedObject.setColor(Color.White);
 					m_selectedObject = null;
 				}
-				Rectangle t_mouseClick = new Rectangle((int)t_worldMouse.X, (int)t_worldMouse.Y, 1, 1);
+				Rectangle t_mouseClick = new Rectangle((int)m_worldMouse.X, (int)m_worldMouse.Y, 1, 1);
 
 				foreach (GameObject t_gameObject in m_gameObjectList)
 				{
@@ -131,25 +180,69 @@ namespace GrandLarceny
 		private void updateMouseDrag(Vector2 a_worldMouse)
 		{
 			if (a_worldMouse.X % 72 >= 36)
-			{
 				a_worldMouse.X = a_worldMouse.X + (72 - (a_worldMouse.X % 72));	
-			}
 			else if (a_worldMouse.X % 72 < 36)
-			{
 				a_worldMouse.X = a_worldMouse.X - (a_worldMouse.X % 72);
-			}
 
 			if (a_worldMouse.Y % 72 >= 36)
-			{
 				a_worldMouse.Y = a_worldMouse.Y + (72 - (a_worldMouse.Y % 72));
-			}
 			else if (a_worldMouse.Y % 72 < 36)
-			{
 				a_worldMouse.Y = a_worldMouse.Y - (a_worldMouse.Y % 72);
-			}
 			
 			m_selectedObject.getPosition().setX((a_worldMouse.X - (m_selectedObject.getImg().getSize().X / 2)) + 36);
 			m_selectedObject.getPosition().setY((a_worldMouse.Y - (m_selectedObject.getImg().getSize().Y / 2)) + 36);
+		}
+
+		private void createPlatform()
+		{
+			Platform t_platform = new Platform(m_worldMouse, "Images//tile");
+
+			if (t_platform.getLeftPoint() % 72 >= 36)
+				t_platform.setLeftPoint(t_platform.getLeftPoint() + (72 - (t_platform.getLeftPoint() % 72)) - 36);
+			else if (t_platform.getLeftPoint() % 72 < 36)
+				t_platform.setLeftPoint(t_platform.getLeftPoint() - (t_platform.getLeftPoint() % 72) + 36);
+
+			if (t_platform.getTopPoint() % 72 >= 36)
+				t_platform.setTopPoint(t_platform.getTopPoint() + (72 - (t_platform.getTopPoint() % 72)) - 36);
+			else if (t_platform.getTopPoint() % 72 < 36)
+				t_platform.setTopPoint(t_platform.getTopPoint() - (t_platform.getTopPoint() % 72) + 36);
+
+			m_gameObjectList.AddLast(t_platform);
+		}
+
+		private void createLadder()
+		{
+			Ladder t_ladder = new Ladder(m_worldMouse, "Images//tile");
+
+			if (t_ladder.getLeftPoint() % 72 >= 36)
+				t_ladder.setLeftPoint(t_ladder.getLeftPoint() + (72 - (t_ladder.getLeftPoint() % 72)) - 36);
+			else if (t_ladder.getLeftPoint() % 72 < 36)
+				t_ladder.setLeftPoint(t_ladder.getLeftPoint() - (t_ladder.getLeftPoint() % 72) + 36);
+
+			if (t_ladder.getTopPoint() % 72 >= 36)
+				t_ladder.setTopPoint(t_ladder.getTopPoint() + (72 - (t_ladder.getTopPoint() % 72)) - 36);
+			else if (t_ladder.getTopPoint() % 72 < 36)
+				t_ladder.setTopPoint(t_ladder.getTopPoint() - (t_ladder.getTopPoint() % 72) + 36);
+
+			m_gameObjectList.AddLast(t_ladder);
+		}
+
+		private void createBackground()
+		{
+			Environment t_environment = new Environment(m_worldMouse, "Images//test");
+
+			if (t_environment.getLeftPoint() % 72 >= 36)
+				t_environment.setLeftPoint(t_environment.getLeftPoint() + (72 - (t_environment.getLeftPoint() % 72)) - 36);
+			else if (t_environment.getLeftPoint() % 72 < 36)
+				t_environment.setLeftPoint(t_environment.getLeftPoint() - (t_environment.getLeftPoint() % 72) + 36);
+
+			if (t_environment.getTopPoint() % 72 >= 36)
+				t_environment.setTopPoint(t_environment.getTopPoint() + (72 - (t_environment.getTopPoint() % 72)) - 36);
+			else if (t_environment.getTopPoint() % 72 < 36)
+				t_environment.setTopPoint(t_environment.getTopPoint() - (t_environment.getTopPoint() % 72) + 36);
+			
+			t_environment.setLayer(1);
+			m_gameObjectList.AddLast(t_environment);
 		}
 
 		public override void draw(GameTime a_gameTime, SpriteBatch a_spriteBatch)
