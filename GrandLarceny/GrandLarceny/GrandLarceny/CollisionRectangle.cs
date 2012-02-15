d484d00eb2b2364dde0a1401a93fcce66961ef80
Yuma Shimizu <yumashimizu90@gmail.com>
2012-02-15 11:42:02 +0100

    outBox finns nu i collisionshape

--- GrandLarceny/GrandLarceny/GrandLarceny/CollisionRectangle.cs
+++ GrandLarceny/GrandLarceny/GrandLarceny/CollisionRectangle.cs
@@ -12,23 +12,28 @@ namespace GrandLarceny
 		public float m_y;
 		public float m_width;
 		public float m_height;
-		
+
 		public CollisionRectangle(float a_x, float a_y, float a_width, float a_height)
 		{
 			m_x = a_x;
 			m_y = a_y;
 			m_width = a_width;
 			m_height = a_height;
+			OutBox = new Rectangle((int)m_x, (int)m_y, (int)m_width, (int)m_height);
 		}
 		public void setPosition(Vector2 a_pos)
 		{
 			m_x = a_pos.X;
 			m_y = a_pos.Y;
+			OutBox.X = (int)m_x;
+			OutBox.Y = (int)m_y;
 		}
 		public void setSize(Vector2 a_size)
 		{
 			m_width = a_size.X;
 			m_height = a_size.Y;
+			OutBox.Width = (int)m_width;
+			OutBox.Height = (int)m_height;
 		}
 
 		public override bool Collides(CollisionShape a_cs)
--- GrandLarceny/GrandLarceny/GrandLarceny/CollisionShape.cs
+++ GrandLarceny/GrandLarceny/GrandLarceny/CollisionShape.cs
@@ -2,11 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
 
 namespace GrandLarceny
 {
 	public abstract class CollisionShape
 	{
+		protected Rectangle OutBox;
+
 		public abstract bool Collides(CollisionShape a_cs2);
+
+		public Rectangle getOutBox()
+		{
+			return OutBox;
+		}
 	}
 }
