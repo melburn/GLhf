8ba25d17707a672cab1e902010627528030bcf39
unknown <ant-lind@.win.dsv.su.se>
2012-02-15 15:40:20 +0100

    lite triangel action

--- GrandLarceny/GrandLarceny/GrandLarceny/CollisionRectangle.cs
+++ GrandLarceny/GrandLarceny/GrandLarceny/CollisionRectangle.cs
@@ -52,12 +52,5 @@ namespace GrandLarceny
 			}
 			return false;
 		}
-		public bool contains(Vector2 a_point)
-		{
-			return a_point.X <= m_x + m_width &&
-				a_point.X >= m_x &&
-				a_point.Y <= m_y + m_height &&
-				a_point.Y >= m_y;
-		}
 	}
 }
