0a17dbc6347a3787d3722d9b93ba85ce9d98600d
Melburn <Melburn1@hotmail.com>
2012-02-22 14:32:29 +0100

    lagger till nya filen

---
+++ GrandLarceny/GrandLarceny/GrandLarceny/NonMovingObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace GrandLarceny
+{
+	[Serializable()]
+	class NonMovingObject : Entity
+	{
+		public NonMovingObject(Vector2 a_posV2, String a_sprite, float a_layer) : base(a_posV2, a_sprite, a_layer)
+		{
+			
+		}
+
+		
+	}
+}
