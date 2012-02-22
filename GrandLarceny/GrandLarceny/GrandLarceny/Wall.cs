84edb4d0a4e91293dbd2f608bb34f455dae9c2ac
Melburn <Melburn1@hotmail.com>
2012-02-22 14:31:39 +0100

    bot namn pa nonmovingplatform till nonmovingobject

--- GrandLarceny/GrandLarceny/GrandLarceny/Wall.cs
+++ GrandLarceny/GrandLarceny/GrandLarceny/Wall.cs
@@ -7,7 +7,7 @@ using Microsoft.Xna.Framework;
 namespace GrandLarceny
 {
 	[Serializable()]
-	class Wall : NonMovingPlatform
+	class Wall : NonMovingObject
 	{
 		public Wall(Vector2 a_posV2, String a_sprite, float a_layer) : base(a_posV2, a_sprite, a_layer)
 		{
