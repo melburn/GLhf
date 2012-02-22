9bf86aefabedb9ccb50b217cb3b759c541fa582f
Yuma Shimizu <yumashimizu90@gmail.com>
2012-02-22 14:50:25 +0100

    ok, sliding blev lost med bool variabel, strukturen for kollision andrades lite

--- GrandLarceny/GrandLarceny/GrandLarceny/Ladder.cs
+++ GrandLarceny/GrandLarceny/GrandLarceny/Ladder.cs
@@ -19,21 +19,24 @@ namespace GrandLarceny
 		{
 			if (a_collid is Player)
 			{
-				Player t_player = (Player)a_collid;
-				//Colliding with ze ladd0rz
-				Rectangle t_rect = new Rectangle(getHitBox().getOutBox().X - 2,
-					(int)getPosition().getGlobalY(), 4, getHitBox().getOutBox().Height);
-				if (t_rect.Contains((int)t_player.getLastPosition().X, (int)t_player.getLastPosition().Y))
+				if (CollisionManager.Collides(this.getHitBox(), a_collid.getHitBox()))
 				{
-					if (Keyboard.GetState().IsKeyDown(Keys.Up))
+					Player t_player = (Player)a_collid;
+					//Colliding with ze ladd0rz
+					Rectangle t_rect = new Rectangle(getHitBox().getOutBox().X - 2,
+						(int)getPosition().getGlobalY(), 4, getHitBox().getOutBox().Height);
+					if (t_rect.Contains((int)t_player.getLastPosition().X, (int)t_player.getLastPosition().Y))
 					{
-						t_player.setSpeedX(0);
-						t_player.setState(Player.State.Climbing);
-						if (t_player.getSpeed().Y < -Player.CLIMBINGSPEED || t_player.getSpeed().Y > Player.CLIMBINGSPEED)
-							t_player.setSpeedY(0);
-						t_player.getPosition().setX(getPosition().getGlobalX());
+						if (Keyboard.GetState().IsKeyDown(Keys.Up))
+						{
+							t_player.setSpeedX(0);
+							t_player.setState(Player.State.Climbing);
+							if (t_player.getSpeed().Y < -Player.CLIMBINGSPEED || t_player.getSpeed().Y > Player.CLIMBINGSPEED)
+								t_player.setSpeedY(0);
+							t_player.getPosition().setX(getPosition().getGlobalX());
+						}
+
 					}
-			
 				}
 			}
 
--- GrandLarceny/GrandLarceny/GrandLarceny/Wall.cs
+++ GrandLarceny/GrandLarceny/GrandLarceny/Wall.cs
@@ -19,30 +19,41 @@ namespace GrandLarceny
 			if (a_collid is Player)
 			{
 				Player t_player = (Player)a_collid;
-				//Colliding with ze left Wall wall
-				if ((int)t_player.getLastPosition().X + 1 >= (int)getLastPosition().X + getHitBox().getOutBox().Width)
+				if (CollisionManager.Collides(this.getHitBox(), a_collid.getHitBox()))
 				{
-					t_player.getPosition().setX(getPosition().getGlobalX() + getHitBox().getOutBox().Width);
-					if (t_player.getCurrentState() == Player.State.Jumping)
+					
+					//Colliding with ze left Wall wall
+					if ((int)t_player.getLastPosition().X + 1 >= (int)getLastPosition().X + getHitBox().getOutBox().Width)
 					{
-						t_player.setState(Player.State.Slide);
-						t_player.setFacingRight(true);
-					}
-					t_player.setSpeedX(0);
+						t_player.getPosition().setX(getPosition().getGlobalX() + getHitBox().getOutBox().Width);
+						if (t_player.getCurrentState() == Player.State.Jumping)
+						{
+							t_player.setState(Player.State.Slide);
+							t_player.setFacingRight(true);
+						}
+						t_player.setSpeedX(0);
 
-				}
-				//Colliding with ze right Wall wall
-				else if ((int)t_player.getLastPosition().X + getHitBox().getOutBox().Width - 1 <= (int)getLastPosition().X)
-				{
-					t_player.getPosition().setX(getPosition().getGlobalX() - t_player.getHitBox().getOutBox().Width);
-					if (t_player.getCurrentState() == Player.State.Jumping)
-					{
-						t_player.setState(Player.State.Slide);
-						t_player.setFacingRight(false);
 					}
-					t_player.setSpeedX(0);
-			
+					//Colliding with ze right Wall wall
+					else if ((int)t_player.getLastPosition().X + getHitBox().getOutBox().Width - 1 <= (int)getLastPosition().X)
+					{
+						t_player.getPosition().setX(getPosition().getGlobalX() - t_player.getHitBox().getOutBox().Width);
+						if (t_player.getCurrentState() == Player.State.Jumping)
+						{
+							t_player.setState(Player.State.Slide);
+							t_player.setFacingRight(false);
+						}
+						t_player.setSpeedX(0);
+
+					}
+					//Colliding with ze zeeling
+					else if ((int)t_player.getLastPosition().Y >= (int)getLastPosition().Y + getHitBox().getOutBox().Height)
+					{
+						t_player.getPosition().setY(getPosition().getGlobalY() + getHitBox().getOutBox().Height);
+						t_player.setSpeedY(0);
+					}
 				}
+				t_player.setCollidedWithWall(true);
 			}
 		}
 	}
