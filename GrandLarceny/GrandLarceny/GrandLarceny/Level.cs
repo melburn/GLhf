21f5c589b45cd975be67a03b8369337619c3ae06
Melburn <Melburn1@hotmail.com>
2012-03-01 11:39:13 +0100

    trasig kod, moblerar om objectlistor

--- GrandLarceny/GrandLarceny/GrandLarceny/Level.cs
+++ GrandLarceny/GrandLarceny/GrandLarceny/Level.cs
@@ -10,31 +10,31 @@ namespace GrandLarceny
 	[Serializable]
 	class Level : ISerializable
 	{
-		private LinkedList<GameObject> m_loadedGameObject;
+		private LinkedList<GameObject>[] m_loadedList;
 
 		public Level()
 		{
-			m_loadedGameObject = new LinkedList<GameObject>();
+			m_loadedList = new LinkedList<GameObject>[5];
 		}
 
-		public void setLevelObjects(LinkedList<GameObject> a_gameObjects)
+		public void setLevelObjects(LinkedList<GameObject>[] a_gameObjects)
 		{
-			m_loadedGameObject = a_gameObjects;
+			m_loadedList = a_gameObjects;
 		}
 
-		public LinkedList<GameObject> getLevelObjects()
+		public LinkedList<GameObject>[] getLevelLists()
 		{
-			return m_loadedGameObject;
+			return m_loadedList;
 		}
 
 		public Level(SerializationInfo info, StreamingContext context)
 		{
-			m_loadedGameObject = (LinkedList<GameObject>)info.GetValue("GameObjects", typeof(LinkedList<GameObject>));
+			m_loadedList = (LinkedList<GameObject>[])info.GetValue("GameObjects", typeof(LinkedList<GameObject>[]));
 		}
 
 		public void GetObjectData(SerializationInfo info, StreamingContext context)
 		{
-			info.AddValue("GameObjects", m_loadedGameObject);
+			info.AddValue("GameObjects", m_loadedList);
 		}
 
 	}
