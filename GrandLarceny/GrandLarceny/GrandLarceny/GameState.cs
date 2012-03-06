55291e072fe03a973b457737db879bd8d9cfdcfd
Melburn <Melburn1@hotmail.com>
2012-03-06 16:08:38 +0100

    fixat ventilation och lite med keyboard och sant

--- GrandLarceny/GrandLarceny/GrandLarceny/GameState.cs
+++ GrandLarceny/GrandLarceny/GrandLarceny/GameState.cs
@@ -192,5 +192,13 @@ namespace GrandLarceny
 			}
 			Game.getInstance().m_camera.setLayer(a_newLayer);
 		}
+		public static bool isKeyPressed(Keys key)
+		{
+			return m_currentKeyInput.IsKeyDown(key);
+		}
+		public static bool wasKeyPressed(Keys key)
+		{
+			return m_previousKeyInput.IsKeyDown(key);
+		}
 	}
 }
--- GrandLarceny/GrandLarceny/GrandLarceny/Player.cs
+++ GrandLarceny/GrandLarceny/GrandLarceny/Player.cs
@@ -50,6 +50,8 @@ namespace GrandLarceny
 		private bool m_facingRight = false;
 		private bool m_collidedWithWall = false;
 
+		private Keys m_actionKey = Keys.A;
+
 		public enum Direction
 		{
 			None,
@@ -171,17 +173,17 @@ namespace GrandLarceny
 
 		private void updateStop(float a_deltaTime)
 		{
-			if (GameState.m_previousKeyInput.IsKeyUp(Keys.Z) && GameState.m_currentKeyInput.IsKeyDown(Keys.Z))
+			if (!GameState.wasKeyPressed(Keys.Z) && GameState.isKeyPressed(Keys.Z))
 			{
 				m_currentState = State.Rolling;
 				m_rollTimer = 0.3f;
 				return;
 			}
-			if (GameState.m_currentKeyInput.IsKeyDown(Keys.Left) || GameState.m_currentKeyInput.IsKeyDown(Keys.Right))
+			if (GameState.isKeyPressed(Keys.Left) || GameState.isKeyPressed(Keys.Right))
 			{
 				m_currentState = State.Walking;
 
-				if (GameState.m_currentKeyInput.IsKeyDown(Keys.Left))
+				if (GameState.isKeyPressed(Keys.Left))
 				{
 					m_facingRight = false;
 				}
@@ -190,7 +192,7 @@ namespace GrandLarceny
 					m_facingRight = true;
 				}
 			}
-			if (GameState.m_previousKeyInput.IsKeyUp(Keys.X) && GameState.m_currentKeyInput.IsKeyDown(Keys.X))
+			if (!GameState.wasKeyPressed(Keys.X) && GameState.isKeyPressed(Keys.X))
 			{
 				m_speed.Y -= JUMPSTRENGTH;
 				m_currentState = State.Jumping;
@@ -202,7 +204,7 @@ namespace GrandLarceny
 
 		private void updateWalking(float a_deltaTime)
 		{
-			if (GameState.m_previousKeyInput.IsKeyUp(Keys.Z) && GameState.m_currentKeyInput.IsKeyDown(Keys.Z))
+			if (!GameState.wasKeyPressed(Keys.Z) && GameState.isKeyPressed(Keys.Z))
 			{
 
 				m_currentState = State.Rolling;
@@ -210,7 +212,7 @@ namespace GrandLarceny
 				return;
 			}
 
-			if (GameState.m_currentKeyInput.IsKeyDown(Keys.Right))
+			if (GameState.isKeyPressed(Keys.Right))
 			{
 				if (m_speed.X > PLAYERSPEED)
 				{
@@ -218,7 +220,7 @@ namespace GrandLarceny
 				}
 				else
 				{
-					if (GameState.m_currentKeyInput.IsKeyDown(Keys.LeftShift))
+					if (GameState.isKeyPressed(Keys.LeftShift))
 					{
 						m_speed.X = Math.Min(m_speed.X + (ACCELERATION * a_deltaTime), 200);
 					}
@@ -228,7 +230,7 @@ namespace GrandLarceny
 					}
 				}
 			}
-			if (GameState.m_currentKeyInput.IsKeyDown(Keys.Left))
+			if (GameState.isKeyPressed(Keys.Left))
 			{
 				if (m_speed.X < -PLAYERSPEED)
 				{
@@ -236,7 +238,7 @@ namespace GrandLarceny
 				}
 				else
 				{
-					if (GameState.m_currentKeyInput.IsKeyDown(Keys.LeftShift))
+					if (GameState.isKeyPressed(Keys.LeftShift))
 					{
 						m_speed.X = Math.Max(m_speed.X - (ACCELERATION * a_deltaTime), -200);
 					}
@@ -261,7 +263,7 @@ namespace GrandLarceny
 				m_currentState = State.Stop;
 
 			}
-			if (GameState.m_previousKeyInput.IsKeyUp(Keys.X) && GameState.m_currentKeyInput.IsKeyDown(Keys.X))
+			if (!GameState.wasKeyPressed(Keys.X) && GameState.isKeyPressed(Keys.X))
 			{
 				m_speed.Y -= JUMPSTRENGTH;
 				m_currentState = State.Jumping;
@@ -286,7 +288,7 @@ namespace GrandLarceny
 				else if (m_speed.X < 0)
 					m_speed.X = Math.Min(m_speed.X + (AIRDEACCELERATION * a_deltaTime), 0);
 			}
-			else if (GameState.m_currentKeyInput.IsKeyDown(Keys.Left))
+			else if (GameState.isKeyPressed(Keys.Left))
 			{
 				if (m_speed.X < -PLAYERSPEED)
 				{
@@ -297,7 +299,7 @@ namespace GrandLarceny
 					m_speed.X = Math.Max(-PLAYERSPEED, m_speed.X - AIRDEACCELERATION * a_deltaTime);
 				}
 			}
-			else if (GameState.m_currentKeyInput.IsKeyDown(Keys.Right))
+			else if (GameState.isKeyPressed(Keys.Right))
 			{
 				if (m_speed.X > PLAYERSPEED)
 				{
@@ -318,7 +320,7 @@ namespace GrandLarceny
 				m_facingRight = false;
 			}
 
-			if (m_speed.Y < -300 && GameState.m_currentKeyInput.IsKeyDown(Keys.X))
+			if (m_speed.Y < -300 && GameState.isKeyPressed(Keys.X))
 				m_speed.Y -= AIRVERTICALACCELERATION;
 
 			m_cameraPoint.X = Math.Max(Math.Min(m_cameraPoint.X + (m_speed.X * 1.5f * a_deltaTime), CAMERAMAXDISTANCE), -CAMERAMAXDISTANCE);
@@ -328,10 +330,10 @@ namespace GrandLarceny
 		{
 			if (m_lastPosition.Y != m_position.getGlobalY())
 			{
-				if (((!m_facingRight && GameState.m_currentKeyInput.IsKeyDown(Keys.Right)) || (m_facingRight && GameState.m_currentKeyInput.IsKeyDown(Keys.Left)))
+				if (((!m_facingRight && GameState.isKeyPressed(Keys.Right)) || (m_facingRight && GameState.isKeyPressed(Keys.Left)))
 					&& m_collidedWithWall)
 				{
-					if (GameState.m_previousKeyInput.IsKeyUp(Keys.X) && GameState.m_currentKeyInput.IsKeyDown(Keys.X))
+					if (!GameState.wasKeyPressed(Keys.X) && GameState.isKeyPressed(Keys.X))
 					{
 						m_speed.Y = -JUMPSTRENGTH;
 						if (m_facingRight == true)
@@ -354,11 +356,11 @@ namespace GrandLarceny
 		private void updateClimbing()
 		{
 			m_gravity = 0;
-			if (GameState.m_currentKeyInput.IsKeyDown(Keys.Up))
+			if (GameState.isKeyPressed(Keys.Up))
 			{
 				m_speed.Y = -CLIMBINGSPEED;
 			}
-			else if (GameState.m_currentKeyInput.IsKeyDown(Keys.Down))
+			else if (GameState.isKeyPressed(Keys.Down))
 			{
 				m_speed.Y = CLIMBINGSPEED;
 			}
@@ -366,9 +368,9 @@ namespace GrandLarceny
 			{
 				m_speed.Y = 0;
 			}
-			if (GameState.m_currentKeyInput.IsKeyDown(Keys.X) && GameState.m_previousKeyInput.IsKeyUp(Keys.X))
+			if (GameState.isKeyPressed(Keys.X) && !GameState.wasKeyPressed(Keys.X))
 			{
-				if (!GameState.m_currentKeyInput.IsKeyDown(Keys.Down))
+				if (!GameState.isKeyPressed(Keys.Down))
 				{
 					m_speed.Y = -JUMPSTRENGTH;
 					if (m_facingRight)
@@ -406,7 +408,7 @@ namespace GrandLarceny
 			m_rollTimer -= a_deltaTime;
 
 
-			if ((GameState.m_previousKeyInput.IsKeyUp(Keys.X) && GameState.m_currentKeyInput.IsKeyDown(Keys.X)) || m_rollTimer <= 0)
+			if ((!GameState.wasKeyPressed(Keys.X) && GameState.isKeyPressed(Keys.X)) || m_rollTimer <= 0)
 			{
 				if (m_rollTimer <= 0)
 				{
@@ -438,9 +440,9 @@ namespace GrandLarceny
 		private void updateHanging()
 		{
 			m_gravity = 0;
-			if (GameState.m_previousKeyInput.IsKeyUp(Keys.X) && GameState.m_currentKeyInput.IsKeyDown(Keys.X))
+			if (!GameState.wasKeyPressed(Keys.X) && GameState.isKeyPressed(Keys.X))
 			{
-				if (GameState.m_currentKeyInput.IsKeyDown(Keys.Down))
+				if (GameState.isKeyPressed(Keys.Down))
 				{
 					m_position.plusYWith(1);
 					m_currentState = State.Jumping;
@@ -468,11 +470,11 @@ namespace GrandLarceny
 					m_currentState = State.Jumping;
 				}
 			}
-			else if (GameState.m_currentKeyInput.IsKeyDown(Keys.Down) && m_ladderDirection != Direction.None)
+			else if (GameState.isKeyPressed(Keys.Down) && m_ladderDirection != Direction.None)
 			{
 				m_currentState = State.Climbing;
 			}
-			else if (GameState.m_currentKeyInput.IsKeyDown(Keys.Up) && GameState.m_previousKeyInput.IsKeyUp(Keys.Up))
+			else if (GameState.isKeyPressed(Keys.Up) && !GameState.wasKeyPressed(Keys.Up))
 			{
 				m_currentState = State.Stop;
 				
@@ -487,9 +489,9 @@ namespace GrandLarceny
 
 		private void updateHiding()
 		{
-			if ((GameState.m_currentKeyInput.IsKeyDown(Keys.Up) && GameState.m_previousKeyInput.IsKeyUp(Keys.Up))
-				|| (GameState.m_currentKeyInput.IsKeyDown(Keys.Down) && GameState.m_previousKeyInput.IsKeyUp(Keys.Down))
-				|| (GameState.m_currentKeyInput.IsKeyDown(Keys.X) && GameState.m_previousKeyInput.IsKeyUp(Keys.X)))
+			if ((GameState.isKeyPressed(Keys.Up) && !GameState.wasKeyPressed(Keys.Up))
+				|| (GameState.isKeyPressed(Keys.Down) && !GameState.wasKeyPressed(Keys.Down))
+				|| (GameState.isKeyPressed(Keys.X) && !GameState.wasKeyPressed(Keys.X)))
 			{
 				m_currentState = State.Stop;
 				m_layer = m_originalLayer;
