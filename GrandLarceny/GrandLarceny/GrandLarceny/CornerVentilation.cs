642db4d91950aeb0752fd0a9c00e51d0a74d4256
Melburn <Melburn1@hotmail.com>
2012-03-06 16:32:44 +0100

    lol

---
+++ GrandLarceny/GrandLarceny/GrandLarceny/CornerVentilation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace GrandLarceny
+{
+	class CornerVentilation : NonMovingObject
+	{
+		public CornerVentilation(Vector2 a_posV2, String a_sprite, float a_layer) : base(a_posV2, a_sprite, a_layer)
+		{
+			
+		}
+	}
+}
