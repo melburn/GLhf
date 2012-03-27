142661a1989dd86fa9904ce5f3a3e958cbeff107
Melburn <Melburn1@hotmail.com>
2012-03-27 10:48:51 +0200

    brjat pa rep

---
+++ GrandLarceny/GrandLarceny/GrandLarceny/Rope.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace GrandLarceny
+{
+	class Rope : NonMovingObject
+	{
+		public Rope(Vector2 a_posV2, String a_sprite, float a_layer)
+			: base(a_posV2, a_sprite, a_layer)
+		{
+
+		}
+		public override void loadContent()
+		{
+			base.loadContent();
+			m_rotationPoint = new Vector2(m_img.getSize().X / 2, m_position.getGlobalY());
+		}
+	}
+}
