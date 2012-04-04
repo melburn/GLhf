12668bd1b32f3c62e17715ed56ae69a27dbce1a9
Anton Linden <anton.linden@gmail.com>
2012-04-04 15:53:45 +0200

    addade

---
+++ GrandLarceny/GrandLarceny/GrandLarceny/Events/Triggers/ChaseTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GrandLarceny.Events.Triggers
+{
+	[Serializable()]
+	public class ChaseTrigger : EventTrigger
+	{
+		private bool m_condition;
+
+		public ChaseTrigger(bool a_condition)
+		{
+			m_condition = a_condition;
+		}
+		public override bool isTrue()
+		{
+			return Game.getInstance().getState().getPlayer().isChase() == m_condition;
+		}
+
+		public override string ToString()
+		{
+			return "chase: " + m_condition;
+		}
+	}
+}
