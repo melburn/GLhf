a9ce59d6ac9f3acf53d103495ace8b604e5903ab
Melburn <Melburn1@hotmail.com>
2012-04-24 15:51:03 +0200

    lagt till sista ventialtionstypen

---
+++ GrandLarceny/GrandLarceny/GrandLarceny/VentilationEnd.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace GrandLarceny
+{
+	class VentilationEnd : Ventilation
+	{
+		public VentilationEnd(Vector2 a_posV2, String a_sprite, float a_layer) : base(a_posV2, a_sprite, a_layer)
+		{
+			
+		}
+
+		internal override void updateCollisionWith(Entity a_collider)
+		{
+			if (a_collider is Player)
+			{
+				Player t_player = (Player)a_collider;
+				if (CollisionManager.Contains(getHitBox(), t_player.getPosition().getGlobalCartesianCoordinates()))
+				{
+					if (0 == Math.Round(((2 * m_rotate) / Math.PI) % 4))
+					{
+						t_player.setVentilationDirection(m_leftList);
+					}
+					else if (1 == Math.Round(((2 * m_rotate) / Math.PI) % 4))
+					{
+						t_player.setVentilationDirection(m_downList);
+					}
+					else if (2 == Math.Round(((2 * m_rotate) / Math.PI) % 4))
+					{
+						t_player.setVentilationDirection(m_rightList);
+					}
+					else if (3 == Math.Round(((2 * m_rotate) / Math.PI) % 4))
+					{
+						t_player.setVentilationDirection(m_upList);
+					}
+					t_player.setVentilationObject(this);
+				}
+			}
+		}
+	}
+}
