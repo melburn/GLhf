ea31710b2e5402b54068c2832d935a831f8c7bc6
Anton Linden <anton.linden@gmail.com>
2012-05-16 15:36:17 +0200

    fixat ventiler sa att man inte kan ut ur dem

--- GrandLarceny/GrandLarceny/GrandLarceny/Consumables/ConsumableGoal.cs
+++ GrandLarceny/GrandLarceny/GrandLarceny/Consumables/ConsumableGoal.cs
@@ -20,12 +20,12 @@ namespace GrandLarceny
 		protected override bool collect()
 		{
 			int t_num = 4 - ((GameState)Game.getInstance().getState()).numberOfGoals();
-			if (t_num < 1 || t_num > 3)
+			if (t_num > 0 && t_num < 4)
 			{
 				String t_textureName = "Images//GUI//GameGUI//stolen"+t_num+"of3";
 				Particle t_stolenGoods = new Particle(new CartesianCoordinate(Vector2.Zero, Game.getInstance().m_camera.getPosition()), t_textureName, 33, 0.0015f);
 				t_stolenGoods.getPosition().setLocalCartesian(-t_stolenGoods.getImg().getSize() / 2);
-				t_stolenGoods.setTimer(((float)Game.getInstance().getTotalGameTime().TotalMilliseconds) + 3f);
+				t_stolenGoods.setTimer(((float)Game.getInstance().getTotalGameTime().TotalMilliseconds) + 3000f);
 				Game.getInstance().getState().addObject(t_stolenGoods);
 			}
 			return true;
