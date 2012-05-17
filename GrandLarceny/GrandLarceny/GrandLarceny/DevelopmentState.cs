17845044c899d6e9d350579798829a6b4805054c
Joakim Clysen <stjoakim@gmail.com>
2012-05-17 14:02:45 +0200

    battre musikklass da den kollar om musiken finns nu

--- GrandLarceny/GrandLarceny/GrandLarceny/DevelopmentState.cs
+++ GrandLarceny/GrandLarceny/GrandLarceny/DevelopmentState.cs
@@ -124,8 +124,8 @@ namespace GrandLarceny
 			m_lineList			= new LinkedList<Line>();
 			m_objectPreview		= null;
 
-			m_sndKeyclick		= new Sound("SoundEffects//GUI//button");
-			m_sndSave			= new Sound("SoundEffects//GUI//ZMuFir00");
+			m_sndKeyclick		= new Sound("GUI//button");
+			m_sndSave			= new Sound("GUI//ZMuFir00");
 
 			m_guiList.AddLast(new Text(new Vector2(350, 3)	, "Layer:", "VerdanaBold", Color.Black, false));
 			m_guiList.AddLast(m_parallaxLabel		= new Text(new Vector2(460, 3)	, "Parallax Value:", "VerdanaBold", Color.Black, false));
