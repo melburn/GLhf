c3451a7a7a860f71fa608fa079750997016c5833
Joakim Clysen <stjoakim@gmail.com>
2012-05-22 11:27:50 +0200

    asdf

--- GrandLarceny/GrandLarceny/GrandLarceny/MenuStates/LevelMenu.cs
+++ GrandLarceny/GrandLarceny/GrandLarceny/MenuStates/LevelMenu.cs
@@ -32,36 +32,24 @@ namespace GrandLarceny
 			m_levelText = new Text(new Vector2(405, 80), "New Level:", "VerdanaBold", Color.White, false);
 			m_newLevelName = new TextField(new Vector2(400, 100), 200, 32, true, true, true, 20);
 			string[] t_ext = { ".lvl" };
-			if (!Directory.Exists("Content//levels//"))
+			if (!Directory.Exists("Content//levels//CustomLevels//"))
 			{
-				System.IO.Directory.CreateDirectory("Content//levels//");
+				System.IO.Directory.CreateDirectory("Content//levels//CustomLevels//");
 			}
-			string[] t_fileList = Directory.GetFiles("Content//levels//");
+			string[] t_fileList = Directory.GetFiles("Content//levels//CustomLevels//");
 			
 			m_buttons = GuiListFactory.createListFromStringArray(t_fileList, t_ext, "btn_test_empty");
 			GuiListFactory.setListPosition(m_buttons, new Vector2(25, 25));
 			GuiListFactory.setTextOffset(m_buttons, new Vector2(10, 10));
 			GuiListFactory.setButtonDistance(m_buttons, new Vector2(0, 60));
 
-			LinkedList<string> t_levelLocked = getLockedLevels();
-
 			foreach (Button t_button in m_buttons)
 			{
-				if (t_levelLocked.Contains(t_button.getText()))
-				{
-					//något event ska addas här det är inte tomt jävla skit ungar
-				}
-				else
-				{
-					t_button.m_clickEvent += new Button.clickDelegate(startLevelClick);
-				}
+				t_button.m_clickEvent += new Button.clickDelegate(toggleLevel);
 			}
 
 			m_buttons.AddLast(m_btnTFAccept = new Button("btn_textfield_accept", new Vector2(600, 100)));
 			m_btnTFAccept.m_clickEvent += new Button.clickDelegate(createNewLevel);
-			Button t_saveProgressButton = new Button("btn_asset_list", new Vector2(500, 400), "Save Game", "VerdanaBold", Color.White, new Vector2(10, 0));
-			t_saveProgressButton.m_clickEvent += new Button.clickDelegate(saveProgressClick);
-			m_buttons.AddLast(t_saveProgressButton);
 		}
 		#endregion
 
@@ -69,7 +57,9 @@ namespace GrandLarceny
 		public override void update(GameTime a_gameTime)
 		{
 			foreach (Button t_b in m_buttons)
+			{
 				t_b.update();
+			}
 			m_newLevelName.update(a_gameTime);
 			if (a_gameTime.TotalGameTime > m_textTimeOut)
 			{
@@ -85,7 +75,9 @@ namespace GrandLarceny
 		public override void draw(GameTime a_gameTime, SpriteBatch a_spriteBatch)
 		{
 			foreach (Button t_b in m_buttons)
+			{
 				t_b.draw(a_gameTime, a_spriteBatch);
+			}
 			m_newLevelName.draw(a_gameTime);
 			m_levelText.draw(a_gameTime);
 		}
@@ -128,34 +120,13 @@ namespace GrandLarceny
 			}
 		}
 
-		public void saveProgressClick(Button a_b)
+		private void toggleLevel(Button a_button)
 		{
-			Game.getInstance().setState(new LoadAndSaveMenu(true, this));
-
-		}
-
-		private LinkedList<string> getLockedLevels()
-		{
-			LinkedList<string> t_LockedLevel = new LinkedList<string>();
-			string[] t_rawLevelLine = System.IO.File.ReadAllLines("Content\\levels\\LevelRequirement.txt");
-			char[] t_splitter = { ':' };
-			foreach (string t_level in t_rawLevelLine)
+			foreach (Button t_button in m_buttons)
 			{
-				string[] t_levelAndReq = t_level.Split(t_splitter);
-				int t_levelLocked = 1;
-				for (int i = 1; i < t_levelAndReq.Length; ++i)
-				{
-					if (Game.getInstance().getProgress().hasClearedLevel(t_levelAndReq[i]))
-					{
-						t_levelLocked++;
-					}
-				}
-				if (t_levelAndReq.Length > t_levelLocked)
-				{
-					t_LockedLevel.AddLast(t_levelAndReq[0]);
-				}
+
 			}
-			return t_LockedLevel;
+			a_button.setState(Button.State.Toggled);
 		}
 		#endregion
 	}
