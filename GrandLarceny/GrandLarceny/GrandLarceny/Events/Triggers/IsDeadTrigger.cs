639a4ea17fe3d3b2afaa8ad547cf7675bde86563
Anton Linden <anton.linden@gmail.com>
2012-05-23 14:01:39 +0200

    fixade link i isDeadTrigger

--- GrandLarceny/GrandLarceny/GrandLarceny/Events/Triggers/IsDeadTrigger.cs
+++ GrandLarceny/GrandLarceny/GrandLarceny/Events/Triggers/IsDeadTrigger.cs
@@ -9,10 +9,17 @@ namespace GrandLarceny.Events.Triggers
 	public class IsDeadTrigger : EventTrigger
 	{
 		private bool m_condition;
+
+		[NonSerialized]
 		private GameObject m_object;
+		private int m_objectLink;
 
 		public IsDeadTrigger(GameObject a_object, bool a_condition)
 		{
+			if (m_object == null)
+			{
+				throw new ArgumentNullException();
+			}
 			m_condition = a_condition;
 			m_object = a_object;
 		}
@@ -31,6 +38,26 @@ namespace GrandLarceny.Events.Triggers
 			else
 			{
 				return m_object + " is dead";
+			}
+		}
+
+		public override void linkObject()
+		{
+			if (m_object != null)
+			{
+				m_objectLink = m_object.getId();
+			}
+		}
+
+		public override void loadContent()
+		{
+			if (m_objectLink > 0)
+			{
+				m_object = (LampSwitch)Game.getInstance().getState().getObjectById(m_objectLink);
+				if (m_object == null)
+				{
+					throw new ArgumentNullException("SwitchTrigger could not find switch " + m_objectLink);
+				}
 			}
 		}
 	}
